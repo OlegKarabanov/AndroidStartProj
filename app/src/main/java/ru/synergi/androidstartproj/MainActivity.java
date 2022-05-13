0d202b51681d20618b4776445a5a57b61b3240c7
OlegKarabanov <salem27@mail.ru>
2022-05-13 15:47:10 +0300

    Added Java screen configuration example with Constraint second_Layout finished

--- app/src/main/java/ru/synergi/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/synergi/androidstartproj/MainActivity.java
@@ -11,26 +11,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_layout);
 
-        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-        TextView textView = new TextView(this);
-        textView.setText("Hello Android!");
-        textView.setTextSize(40);
+    TextView textView = (TextView) findViewById(R.id.header);
 
-        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.WRAP_CONTENT,  // базовые параметры
-                ConstraintLayout.LayoutParams.WRAP_CONTENT);
-//цепляемся к родителю
-        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;   //по левому краю
-        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;     // по верху
-        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;  // по правому краю
-      //  layoutParams.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID; // по низу
-        textView.setLayoutParams(layoutParams);
+    textView.setText("Hello from Java");
 
-        constraintLayout.addView(textView);
 
-        setContentView(constraintLayout);
+//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+//        TextView textView = new TextView(this);
+//        textView.setText("Hello Android!");
+//        textView.setTextSize(40);
+//
+//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT,  // базовые параметры
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+////цепляемся к родителю
+//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;   //по левому краю
+//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;     // по верху
+//        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;  // по правому краю
+//      //  layoutParams.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID; // по низу
+//        textView.setLayoutParams(layoutParams);
+//
+//        constraintLayout.addView(textView);
+//
+//        setContentView(constraintLayout);
 
     }
 }
