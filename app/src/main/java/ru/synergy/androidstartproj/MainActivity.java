cdc45669d7e75de36d1858d6df74e3853c6df78a
OlegKarabanov <salem27@mail.ru>
2022-05-25 19:44:26 +0300

    Added go to inActivity and simple open Activity

---
+++ app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
@@ -0,0 +1,72 @@
+package ru.synergy.androidstartproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private static final int REQ_C = 1;
+    EditText et; // находим текст
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+
+        et = (EditText) findViewById(R.id.et); //находим и кладем здесь
+        tv = (TextView) findViewById(R.id.tv); // находим текствью из 18 строки
+
+
+        Button btn = (Button) findViewById(R.id.button);
+        Button btn2 = (Button) findViewById(R.id.button2);
+        Button btn3 = (Button) findViewById(R.id.button3);
+
+        btn.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+
+
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.button:
+                i= new Intent(this, MainActivity2.class);
+                startActivity(i);
+                break;
+            case R.id.button2:
+                i = new Intent(this, ToInfActivity.class);
+                String eText = et.getText().toString(); // преобразуем в стрингу и отправляем в ет
+                i.putExtra("et", eText);
+                startActivity(i);
+                break;
+//            case R.id.button3: // потом надо имплементировать
+//                i = new Intent(this, ComeBackActivity.class); // вызвали
+    //                startActivityForResult(i, REQ_C); // передали реквест код
+        }
+    }
+
+//    @Override
+//    public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        switch (requestCode) {
+//            case RESULT_OK:
+//                tv.setText(data.getStringExtra("et"));
+//        }
+//        return false;
+//    }
+}
