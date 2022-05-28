5926a30c792f12c6ef12cb3af4d0a46654379f8b
OlegKarabanov <salem27@mail.ru>
2022-05-28 07:51:51 +0300

    Finished with activity comeBack

--- app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
@@ -1,5 +1,10 @@
 package ru.synergy.androidstartproj;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -11,11 +16,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final int REQ_C = 1;
     EditText et; // находим текст
     private TextView tv;
+
+
+    ActivityResultLauncher<Intent> mStartActivityForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Intent intent = result.getData();
+                    tv.setText(intent.getStringExtra("tv"));
+                }
+            }
+
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,13 +68,13 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 break;
             case R.id.button2:
                 i = new Intent(this, ToInfActivity.class);
-                String eText = et.getText().toString(); // преобразуем в стрингу и отправляем в ет
+                String eText = et.getText().toString();
                 i.putExtra("et", eText);
                 startActivity(i);
                 break;
-//            case R.id.button3: // потом надо имплементировать
-//                i = new Intent(this, ComeBackActivity.class); // вызвали
-    //                startActivityForResult(i, REQ_C); // передали реквест код
+            case R.id.button3:
+                      i = new Intent(this, ComeBackActivity.class);
+                      mStartActivityForResult.launch(i);
         }
     }
 
